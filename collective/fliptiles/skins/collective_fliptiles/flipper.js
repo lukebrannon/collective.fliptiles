0bd041f13c168c66863de265c4f9a16809e69fb0
Luke Brannon <luke.brannon@gmail.com>
2013-08-13 22:22:06 -0700

    updated flipper.js

--- collective/fliptiles/skins/collective_fliptiles/flipper.js
+++ collective/fliptiles/skins/collective_fliptiles/flipper.js
@@ -1,17 +1,7 @@
 $(function() {
     
-    $('.quickFlip').quickFlip();
-    
-    for ( var i = 0; i < $.quickFlip.wrappers.length; i++ ) {
-        var thisOne = $.quickFlip.wrappers[i];
-
-    $( thisOne.wrapper ).hover( function(ev) {
-        var $target = $(ev.target);
-
-    if ( !$target.hasClass('quickFlip') ) $target = $target.parent();
-                      
-    $target.quickFlipper();
-                                    
-    }, function() {});
-}
+    $('.quickFlip').hover(
+    		function() {
+    			$(this).quickFlipper();
+    		});
 });
